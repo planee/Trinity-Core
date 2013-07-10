1ce35112699e02c700871261a5c73d031a15e2a5
yolinlin <413681487@qq.com>
2013-07-10 22:32:01 +0800

    更新到 SHA-1: 81d8739d68e2ca2b537e5d4f81f8efff4f4b58e6
    * Core/Scripting: Converted Watchkeeper Gargolmar to use events.
    
    修复一些刷屏

---
+++ sql/Custom/2013-07-10-world.sql
@@ -0,0 +1,15 @@
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 11 and `entryorguid` = 2318;
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 12 and `entryorguid` = 2318;
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 13 and `entryorguid` = 2318;
+
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 16 and `entryorguid` = 2345;
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 17 and `entryorguid` = 2345;
+
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 13 and `entryorguid` = 2346;
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` = 14 and `entryorguid` = 2346;
+
+
+
+
+
+
