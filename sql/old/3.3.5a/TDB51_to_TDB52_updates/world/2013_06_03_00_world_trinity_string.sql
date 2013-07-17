e76520ea44b0752350717671803e24c645fbdfd2
yolinlin <413681487@qq.com>
2013-07-17 10:42:23 +0800

    * TDB 335.52 - 2013/07/17

--- sql/old/3.3.5a/TDB51_to_TDB52_updates/world/2013_06_03_00_world_trinity_string.sql
+++ sql/old/3.3.5a/TDB51_to_TDB52_updates/world/2013_06_03_00_world_trinity_string.sql
@@ -1,9 +1,9 @@
 DELETE FROM `trinity_string` WHERE  `entry`=854 LIMIT 1;
 INSERT INTO `trinity_string` (`entry`, `content_default`, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8) VALUES 
-(854, 'â”” Mails: %d Read/%u Total', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(854, 'â”?Mails: %d Read/%u Total', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 DELETE FROM `trinity_string` WHERE  `entry`=871 LIMIT 1;
 INSERT INTO `trinity_string` (`entry`, `content_default`, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8) VALUES 
-(871, 'â”‚ Level: %u', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(871, 'â”?Level: %u', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-UPDATE `trinity_string` SET `content_default`='â”‚ Level: %u (%u/%u XP (%u XP left))' WHERE  `entry`=843 LIMIT 1;
+UPDATE `trinity_string` SET `content_default`='â”?Level: %u (%u/%u XP (%u XP left))' WHERE  `entry`=843 LIMIT 1;
