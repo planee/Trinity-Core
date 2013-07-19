dfffa45912ba7b26fe8875c06e365aa1c9c28a26
yolinlin <413681487@qq.com>
2013-07-19 09:04:51 +0800

    修复TOC箱子不能点

---
+++ sql/Custom/2013-07-19-world.sql
@@ -0,0 +1,6 @@
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195709;
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195710;
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195374;
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195375;
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195323;
+UPDATE `gameobject_template` SET `flags` = 0 WHERE `entry` = 195324;
