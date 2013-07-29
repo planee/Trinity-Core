1e1a2ff85e10b7e49a3383002b0801c1ae053d1a
yolinlin <413681487@qq.com>
2013-07-29 21:55:57 +0800

    修复“标记哨塔”

---
+++ sql/Custom/2013-07-29-world.sql
@@ -0,0 +1,6 @@
+UPDATE `item_template` SET `spellcharges_1`='0' WHERE (`entry`='12815');
+UPDATE `item_template` SET `spellppmRate_1`='0' WHERE (`entry`='12815');
+UPDATE `quest_template` SET `RequiredItemId1`='12815' WHERE (`Id`='5097');
+UPDATE `quest_template` SET `RequiredItemCount1`='1' WHERE (`Id`='5097');
+UPDATE `quest_template` SET `RequiredItemId1`='12815' WHERE (`Id`='5098');
+UPDATE `quest_template` SET `RequiredItemCount1`='1' WHERE (`Id`='5098');
