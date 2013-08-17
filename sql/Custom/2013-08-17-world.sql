3b46af8a812d84c5bebbfeb7a00a66e3aae8a597
yolinlin <413681487@qq.com>
2013-08-17 08:19:34 +0800

    修复NPC不在视野中

---
+++ sql/Custom/2013-08-17-world.sql
@@ -0,0 +1,10 @@
+-- NPC因位置问题无法正确进入战斗
+UPDATE `creature` SET `position_x`=  7679.9228 WHERE `guid` = 82277;
+UPDATE `creature` SET `position_y`= -5769.0522 WHERE `guid` = 82277;
+UPDATE `creature` SET `position_z`=     3.8328 WHERE `guid` = 82277;
+UPDATE `creature` SET `orientation`=    1.3229 WHERE `guid` = 82277;
+
+UPDATE `creature` SET `position_x`=  7683.3061 WHERE `guid` = 82293;
+UPDATE `creature` SET `position_y`= -5671.0713 WHERE `guid` = 82293;
+UPDATE `creature` SET `position_z`=     1.9480 WHERE `guid` = 82293;
+UPDATE `creature` SET `orientation`=    5.3369 WHERE `guid` = 82293;
